a74b7378c153531f42af9cc47a3e4baf87a59e35
Andrew W. Harn <andrew.harn@broadcom.com>
2021-10-13 18:05:46 -0400

    Add a web help generator

---
+++ src/generate.ts
@@ -0,0 +1,37 @@
+import {readFileSync, writeFileSync} from "fs";
+import {argv} from "process";
+import {join} from "path";
+import {WebHelpGenerator, Imperative, ImperativeConfig, IHandlerResponseApi, HandlerResponse, CommandResponse} from "@zowe/imperative";
+import {getImperativeConfig} from "@zowe/cli";
+
+(async() => {
+    // Set up the environment and build the command tree
+    const zoweVer = "1.25.0"
+    const filePath = argv[2];
+    const localWebHelpDir = join(__dirname, "../", "generatedWebHelp");
+    const fullCommandTree = readFileSync(filePath).toString();
+    const fullCommandTreeJson = JSON.parse(fullCommandTree).data;
+    const CLIImperativeConfig = getImperativeConfig();
+    const fakeHandlerResponse: IHandlerResponseApi = new HandlerResponse(new CommandResponse({silent: true, responseFormat: "default"}));
+
+    // Initialize Imperative and build the web help generator
+    delete CLIImperativeConfig.commandModuleGlobs;
+    await Imperative.init(CLIImperativeConfig);
+    const loadedImperativeConfig = ImperativeConfig.instance;
+    const webHelpGenerator = new WebHelpGenerator(fullCommandTreeJson, loadedImperativeConfig, localWebHelpDir)
+    
+    // Generate the web-help
+    webHelpGenerator.sanitizeHomeDir = true;
+    webHelpGenerator.buildHelp(fakeHandlerResponse);
+
+    // Update the tree data to have our release information
+    const treeDataPath = join(__dirname, "..", "generatedWebHelp", "tree-data.js")
+    const treeData = readFileSync(treeDataPath).toString();
+    const modifiedTreeData = treeData.replace("zowe 0.0.1", `Zowe v${zoweVer}`);
+    writeFileSync(treeDataPath, modifiedTreeData);
+
+    console.log("Output located in " + localWebHelpDir);
+})().catch((error) => {
+    console.log(error);
+    process.exit(1);
+});
