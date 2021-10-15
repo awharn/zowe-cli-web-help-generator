949fdf9bf0758d2fdd3b73daf32e7c690bd0260b
Andrew W. Harn <andrew.harn@broadcom.com>
2021-10-15 13:32:31 -0400

    Add utilities
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

---
+++ scripts/merge.ts
@@ -0,0 +1,127 @@
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+(async() => {
+    // Set up directories
+    const zoweTreeFile = join(__dirname, "..", "commandTree.json");
+    const commandGroupsDirectory = join(__dirname, "..", "commandGroups");
+    const profilesDirectory = join(__dirname, "..", "profiles");
+    const profilesCreateDirectory = join(profilesDirectory, "create");
+    const profilesDeleteDirectory = join(profilesDirectory, "delete");
+    const profilesListDirectory = join(profilesDirectory, "list");
+    const profilesSetDefaultDirectory = join(profilesDirectory, "set-default");
+    const profilesUpdateDirectory = join(profilesDirectory, "update");
+
+    // Read the file
+    const zoweTreeFileJSON = JSON.parse(readFileSync(zoweTreeFile).toString());
+    const commandTree = zoweTreeFileJSON.data;
+
+    // Add the missing command groups
+    const commandGroupsFiles = readdirSync(commandGroupsDirectory);
+    for (const file of commandGroupsFiles) {
+        const commandGroup = JSON.parse(readFileSync(join(commandGroupsDirectory, file)).toString());
+        let conflict = false;
+        for (const compareGroup of commandTree.children) {
+            if (compareGroup.name === commandGroup.name) { conflict = true; }
+        }
+
+        if (!conflict) { 
+            commandTree.children.push(commandGroup); // This is not pushing objects onto the array.....
+            console.log("Command group " + commandGroup.name + " added successfully.");
+        } else { 
+            console.log("Command group " + commandGroup.name + " was not added due to a conflicting command group.");
+        }
+    }
+
+    // Add the missing profiles
+    for (const commandGroup of commandTree.children) {
+        if (commandGroup.name === "profiles") {
+            for (const child of commandGroup.children) {
+                if (child.name === "create") {
+                    const files = readdirSync(profilesCreateDirectory);
+                    for (const file of files) {
+                        const profile = JSON.parse(readFileSync(join(profilesCreateDirectory, file)).toString());
+                        let conflict = false;
+                        for (const compareProfile of child.children) {
+                            if (profile.name === compareProfile.name) { conflict = true; }
+                        }
+                        if (!conflict) { 
+                            child.children.push(profile);
+                            console.log("Profile " + profile.name + " was added to profiles create successfully.");
+                        } else {
+                            console.log("Profile " + profile.name + " was not added to profiles create due to a conflicting profile.");
+                        }
+                    }
+                } else if (child.name === "delete") {
+                    const files = readdirSync(profilesDeleteDirectory);
+                    for (const file of files) {
+                        const profile = JSON.parse(readFileSync(join(profilesDeleteDirectory, file)).toString());
+                        let conflict = false;
+                        for (const compareProfile of child.children) {
+                            if (profile.name === compareProfile.name) { conflict = true; }
+                        }
+                        if (!conflict) { 
+                            child.children.push(profile);
+                            console.log("Profile " + profile.name + " was added to profiles delete successfully.");
+                        } else {
+                            console.log("Profile " + profile.name + " was not added to profiles delete due to a conflicting profile.");
+                        }
+                    }
+                } else if (child.name === "list") {
+                    const files = readdirSync(profilesListDirectory);
+                    for (const file of files) {
+                        const profile = JSON.parse(readFileSync(join(profilesListDirectory, file)).toString());
+                        let conflict = false;
+                        for (const compareProfile of child.children) {
+                            if (profile.name === compareProfile.name) { conflict = true; }
+                        }
+                        if (!conflict) { 
+                            child.children.push(profile);
+                            console.log("Profile " + profile.name + " was added to profiles list successfully.");
+                        } else {
+                            console.log("Profile " + profile.name + " was not added to profiles list due to a conflicting profile.");
+                        }
+                    }
+                } else if (child.name === "set-default") {
+                    const files = readdirSync(profilesSetDefaultDirectory);
+                    for (const file of files) {
+                        const profile = JSON.parse(readFileSync(join(profilesSetDefaultDirectory, file)).toString());
+                        let conflict = false;
+                        for (const compareProfile of child.children) {
+                            if (profile.name === compareProfile.name) { conflict = true; }
+                        }
+                        if (!conflict) { 
+                            child.children.push(profile);
+                            console.log("Profile " + profile.name + " was added to profiles set-default successfully.");
+                        } else {
+                            console.log("Profile " + profile.name + " was not added to profiles set-default due to a conflicting profile.");
+                        }
+                    }
+                } else if (child.name === "update") {
+                    const files = readdirSync(profilesUpdateDirectory);
+                    for (const file of files) {
+                        const profile = JSON.parse(readFileSync(join(profilesUpdateDirectory, file)).toString());
+                        let conflict = false;
+                        for (const compareProfile of child.children) {
+                            if (profile.name === compareProfile.name) { conflict = true; }
+                        }
+                        if (!conflict) { 
+                            child.children.push(profile);
+                            console.log("Profile " + profile.name + " was added to profiles update successfully.");
+                        } else {
+                            console.log("Profile " + profile.name + " was not added to profiles update due to a conflicting profile.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Write data back to file
+    zoweTreeFileJSON.data = commandTree;
+    const dataToWrite = JSON.stringify(zoweTreeFileJSON, (key, value) => (key !== "handler") ? value : "", 2);
+    writeFileSync(zoweTreeFile, dataToWrite);
+})().catch((error) => {
+    console.log(error);
+    process.exit(1);
+});
---
+++ scripts/trim.ts
@@ -0,0 +1,128 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import { join, resolve } from "path";
+
+(async() => {
+    // Read and parse the files
+    const zoweFile = join(__dirname, "..", "zowe.json");
+    const zoweFileJSON = JSON.parse(readFileSync(zoweFile).toString());
+    const zoweTreeFile = join(__dirname, "..", "commandTree.json");
+    const zoweTreeFileJSON = JSON.parse(readFileSync(zoweTreeFile).toString());
+    const commandTree = zoweTreeFileJSON.data;
+    const commandGroups = zoweFileJSON.commandGroups;
+    const profiles = zoweFileJSON.profiles;
+
+    let commandGroupsFound = 0;
+    let profilesFound = 0;
+
+    // Set up the directories, make sure they exist.
+    const commandGroupsDirectory = join(__dirname, "..", "commandGroups");
+    const profilesDirectory = join(__dirname, "..", "profiles");
+    const profilesCreateDirectory = join(profilesDirectory, "create");
+    const profilesDeleteDirectory = join(profilesDirectory, "delete");
+    const profilesListDirectory = join(profilesDirectory, "list");
+    const profilesSetDefaultDirectory = join(profilesDirectory, "set-default");
+    const profilesUpdateDirectory = join(profilesDirectory, "update");
+    if (!existsSync(commandGroupsDirectory)) { mkdirSync(commandGroupsDirectory); }
+    if (!existsSync(profilesDirectory)) { mkdirSync(profilesDirectory); }
+    if (!existsSync(profilesCreateDirectory)) { mkdirSync(profilesCreateDirectory); }
+    if (!existsSync(profilesDeleteDirectory)) { mkdirSync(profilesDeleteDirectory); }
+    if (!existsSync(profilesListDirectory)) { mkdirSync(profilesListDirectory); }
+    if (!existsSync(profilesSetDefaultDirectory)) { mkdirSync(profilesSetDefaultDirectory); }
+    if (!existsSync(profilesUpdateDirectory)) { mkdirSync(profilesUpdateDirectory); }
+
+    // Save the command groups
+    for (const commandGroupSearching of commandGroups) {
+        for (const commandGroup of commandTree.children) {
+            if (commandGroup.name === commandGroupSearching) {
+                // We found the command group. Save it, and break out of the inner for loop.
+                const commandGroupFilePath = join(__dirname, "..", "commandGroups", commandGroupSearching + ".json");
+                writeFileSync(commandGroupFilePath, JSON.stringify(commandGroup, (key, value) => (key !== "handler") ? value : "", 2));
+                console.log("Command Group " + commandGroupSearching + " was found and saved to: " + resolve(commandGroupFilePath));
+                commandGroupsFound++;
+                break;
+            }
+        }
+    }
+
+    // Save the profiles
+    for (const profileSearching of profiles) {
+        let found = 0;
+        for (const commandGroup of commandTree.children) {
+            if (commandGroup.name === "profiles") {
+                // We found the profiles command group - find create, list, delete, set-default, update
+                for (const child of commandGroup.children) {
+                    if (child.name === "create") {
+                        // Profiles create
+                        for (const profileType of child.children) {
+                            if (profileType.name === profileSearching) {
+                                // We found the requested profile. Save it and break out of the inner loop.
+                                const profilesCreateFilePath = join(profilesCreateDirectory, profileSearching + ".json");
+                                writeFileSync(profilesCreateFilePath, JSON.stringify(profileType, (key, value) => (key !== "handler") ? value : "", 2));
+                                console.log("Profile " + profileSearching + " was found and saved to: " + resolve(profilesCreateFilePath));
+                                found++;
+                                break;
+                            }
+                        }
+                    } else if (child.name === "delete") {
+                        // Profiles delete
+                        for (const profileType of child.children) {
+                            if (profileType.name === profileSearching) {
+                                // We found the requested profile. Save it and break out of the inner loop.
+                                const profilesDeleteFilePath = join(profilesDeleteDirectory, profileSearching + ".json");
+                                writeFileSync(profilesDeleteFilePath, JSON.stringify(profileType, (key, value) => (key !== "handler") ? value : "", 2));
+                                console.log("Profile " + profileSearching + " was found and saved to: " + resolve(profilesDeleteFilePath));
+                                found++;
+                                break;
+                            }
+                        }
+                    } else if (child.name === "list") {
+                        // Profiles list
+                        for (const profileType of child.children) {
+                            if (profileType.name === profileSearching + "s") {
+                                // We found the requested profile. Save it and break out of the inner loop.
+                                // Yes, we add s, and that is intended.
+                                const profilesListFilePath = join(profilesListDirectory, profileSearching + ".json");
+                                writeFileSync(profilesListFilePath, JSON.stringify(profileType, (key, value) => (key !== "handler") ? value : "", 2));
+                                console.log("Profile " + profileSearching + " was found and saved to: " + resolve(profilesListFilePath));
+                                found++;
+                                break;
+                            }
+                        }
+                    } else if (child.name === "set-default") {
+                        // Profiles set-default
+                        for (const profileType of child.children) {
+                            if (profileType.name === profileSearching) {
+                                // We found the requested profile. Save it and break out of the inner loop.
+                                const profilesSetDefaultFilePath = join(profilesSetDefaultDirectory, profileSearching + ".json");
+                                writeFileSync(profilesSetDefaultFilePath, JSON.stringify(profileType, (key, value) => (key !== "handler") ? value : "", 2));
+                                console.log("Profile " + profileSearching + " was found and saved to: " + resolve(profilesSetDefaultFilePath));
+                                found++;
+                                break;
+                            }
+                        }
+                    } else if (child.name === "update") {
+                        // Profiles update
+                        for (const profileType of child.children) {
+                            if (profileType.name === profileSearching) {
+                                // We found the requested profile. Save it and break out of the inner loop.
+                                const profilesUpdateFilePath = join(profilesUpdateDirectory, profileSearching + ".json");
+                                writeFileSync(profilesUpdateFilePath, JSON.stringify(profileType, (key, value) => (key !== "handler") ? value : "", 2));
+                                console.log("Profile " + profileSearching + " was found and saved to: " + resolve(profilesUpdateFilePath));
+                                found++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (found === 5) { profilesFound++; }
+    }
+
+    console.log("Script completed.");
+    if (commandGroupsFound !== commandGroups.length) { console.log("Not all requested command groups were found. Please review script output."); }
+    if (profilesFound !== profiles.length) { console.log("Not all requested profile commands were found. Please review script output."); }
+})().catch((error) => {
+    console.log(error);
+    process.exit(1);
+})
