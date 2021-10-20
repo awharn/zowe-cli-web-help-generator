d9d53767d9979c87759c82e0581a3e5382476a65
Andrew W. Harn <andrew.harn@broadcom.com>
2021-10-20 15:35:29 -0400

    Remove requirement for gulp
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-require('ts-node/register');
-
-/**
- * Development related tasks
- */
-const developmentTasks = require("./gulp/DevelopmentTasks");
-gulp.task("updateLicense", developmentTasks.license);
